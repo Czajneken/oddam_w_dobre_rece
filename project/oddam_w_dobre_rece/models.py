0310f06beb2af0be60623134153c1c6c971741e9
Maciej Czaja <czaja.maciej.8@gmail.com>
2024-08-16 14:43:17 +0200

    Model Donation

--- project/oddam_w_dobre_rece/models.py
+++ project/oddam_w_dobre_rece/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
+
 
 class Institution(models.Model):
     TYPES = (
@@ -13,7 +15,17 @@ class Institution(models.Model):
 
     name = models.CharField(max_length=64)
     description = models.TextField
-    type = models.IntegerField(choices=TYPES)
-    categories = models.ManyToManyField(Category, default=1)
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(Category)
 
 
+class Donation(models.Model):
+    quantity = models.SmallIntegerField
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.SmallIntegerField
+    pick_up_date = models.DateField
+    pick_up_time = models.TimeField
+    pick_up_comment = models.TextField
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
