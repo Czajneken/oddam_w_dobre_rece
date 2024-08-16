e38cc97fc49cc33134067f052038be8da7bb58a4
Maciej Czaja <czaja.maciej.8@gmail.com>
2024-08-16 14:05:48 +0200

    Model Institution

--- project/oddam_w_dobre_rece/models.py
+++ project/oddam_w_dobre_rece/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=64)
+
+class Institution(models.Model):
+    TYPES = (
+        (1, "Fundacja"),
+        (2, "Organizacja pozarządowa"),
+        (3, "Zbiórka lokalna")
+    )
+
+    name = models.CharField(max_length=64)
+    description = models.TextField
+    type = models.IntegerField(choices=TYPES)
+    categories = models.ManyToManyField(Category)
+
 
